fb356b45a4d76c6a2f6698eb44057c0566ad2443
Katarzyna Lapinska <katarzyna.lapinska@intel.com>
2019-12-20 15:02:51 +0100

    Fix for creating symlink objects

--- test_utils/filesystem/symlink.py
+++ test_utils/filesystem/symlink.py
@@ -2,10 +2,19 @@
 # Copyright(c) 2019 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
+from core.test_run import TestRun
 from test_utils.filesystem.file import File
 
 
 class Symlink(File):
     def __init__(self, full_path, target):
-        File.__init__(self, full_path)
         self.target = target
+        File.__init__(self, full_path)
+
+    def md5sum(self, binary=True):
+        output = TestRun.executor.run(
+            f"md5sum {'-b' if binary else ''} {self.target}")
+        if output.exit_code != 0:
+            raise Exception(
+                f"Md5sum command execution failed! {output.stdout}\n{output.stderr}")
+        return output.stdout.split()[0]
