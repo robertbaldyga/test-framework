47d4d9d57763d696ef5796b1d072049b16368669
Katarzyna Lapinska <katarzyna.lapinska@intel.com>
2020-04-30 12:34:12 +0200

    Add get symlink target method

--- test_utils/filesystem/symlink.py
+++ test_utils/filesystem/symlink.py
@@ -7,14 +7,16 @@ from test_utils.filesystem.file import File
 
 
 class Symlink(File):
-    def __init__(self, full_path, target):
-        self.target = target
+    def __init__(self, full_path):
         File.__init__(self, full_path)
 
     def md5sum(self, binary=True):
         output = TestRun.executor.run(
-            f"md5sum {'-b' if binary else ''} {self.target}")
+            f"md5sum {'-b' if binary else ''} {self.get_target()}")
         if output.exit_code != 0:
             raise Exception(
                 f"Md5sum command execution failed! {output.stdout}\n{output.stderr}")
         return output.stdout.split()[0]
+
+    def get_target(self):
+        return TestRun.executor.run_expect_success(f"readlink -f {self.full_path}").stdout
