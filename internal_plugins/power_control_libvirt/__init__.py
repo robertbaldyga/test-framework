2e1e81152b9d8849f5410ef463448f2a9026600b
Katarzyna Lapinska <katarzyna.lapinska@intel.com>
2020-08-17 11:15:27 +0200

    Power control libvirt plugin config check
    Signed-off-by: Katarzyna Lapinska <katarzyna.lapinska@intel.com>

--- internal_plugins/power_control_libvirt/__init__.py
+++ internal_plugins/power_control_libvirt/__init__.py
@@ -2,7 +2,7 @@
 # Copyright(c) 2020 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
-
+from connection.local_executor import LocalExecutor
 from connection.ssh_executor import SshExecutor
 from core.test_run import TestRun
 
@@ -10,19 +10,24 @@ from core.test_run import TestRun
 class PowerControlPlugin:
     def __init__(self, params, config):
         print("Power Control LibVirt Plugin initialization")
-        self.config = config
+        try:
+            self.ip = config['ip']
+            self.user = config['user']
+            self.password = config['password']
+        except Exception:
+            raise Exception("Missing fields in config! ('ip', 'user' and 'password' required)")
 
     def pre_setup(self):
         print("Power Control LibVirt Plugin pre setup")
         if self.config['connection_type'] == 'ssh':
             self.executor = SshExecutor(
-                self.config['ip'],
-                self.config['user'],
-                self.config['password'],
+                self.ip,
+                self.user,
+                self.password,
                 self.config.get('port', 22)
             )
         else:
-            cls.executor = LocalExecutor()
+            self.executor = LocalExecutor()
 
     def post_setup(self):
         pass
